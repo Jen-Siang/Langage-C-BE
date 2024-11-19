ed2bdf25551f8775961a08fa96dd4480e4e23c3f
Jason <ljc.universe@gmail.com>
2024-11-19 17:47:48 +0800

    add temp & hum sensor, problem: can't get the value of temp & hum

---
+++ project_name/Application.cpp
@@ -0,0 +1,51 @@
+/*********************************************************************
+ * @file  Apllication.cpp
+ * @author <mettre l'adresse mail ou nom prenom>
+ * @brief Fichier source de l'application
+ *********************************************************************/
+#include "Application.h"
+
+#include <Arduino.h>
+#include <Wire.h>
+#include "SHT31.h"
+
+SHT31 sht31 = SHT31();
+
+Application::Application()
+{
+  // Code
+  ; 
+}
+  
+Application::~Application()
+{
+  // Code
+  ;
+}  
+
+void Application::init(void)
+{
+  // Code
+  Serial.begin(115200);
+  while(!Serial);                 //wait
+  Serial.println("begin...");  
+  sht31.begin();
+  
+}
+
+
+void Application::run(void)
+{
+  // Code
+  float temp = sht31.getTemperature();
+  float hum = sht31.getHumidity();
+  Serial.print("Temp = "); 
+  Serial.print(temp);
+  Serial.println(" C"); //The unit for  Celsius because original arduino don't support speical symbols
+  Serial.print("Hum = "); 
+  Serial.print(hum);
+  Serial.println("%"); 
+  Serial.println();
+  delay(1000);
+  
+}
